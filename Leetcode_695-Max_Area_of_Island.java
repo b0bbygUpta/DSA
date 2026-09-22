5f6044f4ad8a1c3823435f2cd8c67168e39668c6
Bobby Gupta <bobbyg92500@gmail.com>
2026-09-22 23:00:40 +0530

    Add DFS solution for max area of island problem
    This implementation uses depth-first search (DFS) to calculate the maximum area of an island in a grid, marking visited cells to avoid counting them multiple times.

---
+++ Leetcode_695-Max_Area_of_Island.java
@@ -0,0 +1,36 @@
+// -- Leetcode 695 -- 
+// -- Max Area of Island -- 
+
+
+APPROACH I: 
+  
+class Solution {
+    public int maxAreaOfIsland(int[][] grid) {
+       int m=grid.length, n=grid[0].length;
+
+       int maxArea=0;
+       for(int i=0;i<m;i++){   
+        for(int j=0;j<n;j++){
+            if(grid[i][j] == 1){
+                int area=dfs(grid, i, j);
+                maxArea=Math.max(maxArea, area);
+            }
+        }
+       } 
+       return maxArea;
+    }
+
+    private int dfs(int[][] grid, int i, int j){
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j] == 0){
+            return 0;
+        }
+        grid[i][j] = 0; 
+        int area=1;
+        area+=dfs(grid, i+1, j);
+        area+=dfs(grid, i-1, j);
+        area+=dfs(grid, i, j+1);
+        area+=dfs(grid, i, j-1);
+
+        return area;
+    }
+}
