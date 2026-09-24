dfcb7798df4ac934a00215bc4bc39dda0fe85d46
Bobby Gupta <bobbyg92500@gmail.com>
2026-09-24 23:03:32 +0530

    Implement smallestIndex method in Solution class
    Added a new approach to find the smallest index based on the sum of digits.

--- Daily Challenge - September/September_24th_2026-Leetcode_3550.java
+++ Daily Challenge - September/September_24th_2026-Leetcode_3550.java
@@ -54,3 +54,36 @@ class Solution {
         return sum;
     }
 }
+
+APPROACH III: 
+  
+class Solution {
+    public int smallestIndex(int[] nums) {
+        // int ans=-1;
+        for(int i=0;i<nums.length;i++){
+            int t=nums[i]; // ele;
+            int sum=0;
+            while(t>0){
+                sum+=t%10;
+                t/=10;
+            }
+            int a=sum; // findSum(nums[i]);
+            if(a == i){
+                // ans=i;
+                // break;
+                return i;
+            }
+        }
+        return -1; // ans;
+    }
+
+    // private int findSum(int ele){
+    //     int t=ele;
+    //     int sum=0;
+    //     while(t>0){
+    //         sum+=t%10;
+    //         t/=10;
+    //     }
+    //     return sum;
+    // }
+}
