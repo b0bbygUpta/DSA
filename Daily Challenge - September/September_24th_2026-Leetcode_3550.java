5639a52d8b108e743d085b456e8a8b62e2caecb4
Bobby Gupta <bobbyg92500@gmail.com>
2026-09-24 23:04:37 +0530

    Implement smallestIndex method in Solution class
    The method iterates through the array, calculating the sum of the digits of each element and comparing it to the index. If a match is found, the index is returned; otherwise, -1 is returned.

--- Daily Challenge - September/September_24th_2026-Leetcode_3550.java
+++ Daily Challenge - September/September_24th_2026-Leetcode_3550.java
@@ -87,3 +87,37 @@ class Solution {
     //     return sum;
     // }
 }
+
+
+APPROACH IV: 
+  
+class Solution {
+    public int smallestIndex(int[] nums) {
+        // int ans=-1;
+        for(int i=0;i<nums.length;i++){
+            int t=nums[i]; // ele;
+            int sum=0;
+            while(t>0){
+                sum+=t%10;
+                t/=10;
+            }
+            // int a=sum; // findSum(nums[i]);
+            if(sum == i){
+                // ans=i;
+                // break;
+                return i;
+            }
+        }
+        return -1; // ans;
+    }
+
+    // private int findSum(int ele){
+    //     int t=ele;
+    //     int sum=0;
+    //     while(t>0){
+    //         sum+=t%10;
+    //         t/=10;
+    //     }
+    //     return sum;
+    // }
+}
